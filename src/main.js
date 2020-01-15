00b3334217ebf9e2f0bbcebf12c5914c9a402441
StEve Young <2747745470@qq.com>
2020-01-15 12:12:13 +0800

    refactor: vue-cli v4 (#11)

--- src/main.js
+++ src/main.js
@@ -1,19 +1,10 @@
-/**
- * @Author: steve
- * @Date:   2017-Jun-10 11:45:13
- * @Last modified by:   steve
- * @Last modified time: 2017-Jun-10 12:10:58
- */
-
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  render: h => h(App),
-})
+  render: h => h(App)
+}).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,6 @@
-/**
- * @Author: steve
- * @Date:   2017-Jun-10 11:45:13
- * @Last modified by:   steve
- * @Last modified time: 2017-Jun-10 12:21:27
- */
-
 import Vue from 'vue'
 import Router from 'vue-router'
-import Container from '@/components/Container'
+import Container from '@/views/Container'
 
 Vue.use(Router)
 
